67b9b9334440112e81a74cc08fe58239328afaca
Larry Adigun <larryadigun.student@careerdevs.com>
2019-02-11 23:27:33 +0000

    first commit

---
+++ stockmarketapp.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="stock.css">
+    <title> Stock Market App </title>
+  </head>
+
+  <body>
+    <h1>Larrys Stock Market App For Dummies</h1>
+    <h2>Welcome To The Best <a href="https://www.investopedia.com/terms/s/stockmarket.asp" target="_blank">Stock Market</a> Application on the Planet</h2>
+    <h3>Some Things You Need To Know To Get Started</h3>
+    <p>
+      <ul>
+        <li>Buy Low and Sell High</li>
+        <li>There Is No Such Thing As A Sure Thing</li>
+        <li>Get Familiar With Fillings</li>
+        <li>Think Long Term</li>
+        <li>Dividends Are Your Friend</li>
+      </ul>
+
+      <div class="forbes">
+        Visit <a href="https://www.forbes.com/sites/steveschaefer/2016/01/05/10-things-you-absolutely-need-to-know-about-stocks/#4c1ec03f57ff" target="_blank"> Forbes </a> For More.
+      </div>
+
+
+    </p>
+
+<script>
+
+
+</script>
+
+
+
+
+  </body>
+</html>
